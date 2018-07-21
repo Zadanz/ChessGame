ce2f0ef207eb46cf883c873096372a4c930950bb
Quinnvr <35711409+Quinnvr@users.noreply.github.com>
2018-07-21 17:07:56 -0600

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -8,7 +8,43 @@ public class Bishop extends Piece{
 			this.name = "b" + name;
 	}
 	
+	
+	public Bishop(int x, int y, boolean r)
+	{
+		super(x, y, "Bishop", r);
+	}
+	
 	public String drawPiece() {
 		return name;
 	}
-}
+
+	
+	public boolean isAvailable(int initX, int initY, int finX, int finY)
+	{
+		if (!super.isAvailable(int initX, int initY, int finX, int finY))
+		{
+			return false;
+		}
+		
+		if (Math.abs(initX - finX) == Math.abs(initY - finY))
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	public void moveUnit(int x, int y)//the moving of the piece might be put in another class instead
+	{
+		if (b.isAvailable(int initX, int initY, int finX, int finY))
+		{
+			setX(x);
+			setY(y);
+		}
+			
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		Bishop b = new Bishop();
+	}
