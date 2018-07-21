49aae8473b9208dee16927d0d9d0da6f19b4b22a
Zadanz <41347579+Zadanz@users.noreply.github.com>
2018-07-21 00:58:08 -0600

    Update King.java

--- King.java
+++ King.java
@@ -1,4 +1,3 @@
-
 public class King extends Piece{
 	private String name = "K";
 	
@@ -12,4 +11,12 @@ public class King extends Piece{
 	public String drawPiece() {
 		return name;
 	}
+	
+	public boolean canMove(int x, int y){
+		if ((x == this.posX+1 && y == this.posY)||(x == this.posX+1 && y == this.posY+1)||(x == this.posX && y == this.posY+1)||(x == this.posX-1 && y == this.posY)||(x == this.posX-1 && y == this.posY-1)||(x == this.posX && y == this.posY-1)||(x == this.posX+1 && y == this.posY-1)||(x == this.posX-1 && y == this.posY+1))
+			if (array[x,y].inCheck) //used to check whether the move will result in putting the king into check, thereby making it illegal. (This function doesn't exist yet.)
+				return false;
+			else
+				return true;
+	}	
 }
