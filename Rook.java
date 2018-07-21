68acc3bb5e0ccf98474049ffc03755e8e6ec9715
Quinnvr <35711409+Quinnvr@users.noreply.github.com>
2018-07-21 17:33:51 -0600

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,6 +1,5 @@
 
 public class Rook extends Piece{
-	private String name = "R";
 	
 	public Rook(boolean white) {
 		if (white == true)
@@ -9,7 +8,56 @@ public class Rook extends Piece{
 			this.name = "b" + name;
 	}
 	
+	public Rook(int x, int y, boolean r)
+	{
+		super(x, y, "Rook", r);
+	}
+	
 	public String drawPiece() {
 		return name;
 	}
+	
+	public boolean isAvailable(int initX, int initY, int finX, int finY)
+	{
+		if (!super.isAvailable(int initX, int initY, int finX, int finY))
+		{
+			return false;
+		}
+		if ((finX == initX) || (finY == initY))
+		{
+			if (a.emptySpaceCheck())
+			{
+				return true;
+			}
+		}
+		return false;
+		
+	public void moveUnit(int x, int y)//will change the move stuff to another class
+	{
+		if (rk.isAvailable(int initX, int initY, int finX, int finY))
+		{
+			setX(x);
+			setY(y);
+		}
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Rook rk = new Rook();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
