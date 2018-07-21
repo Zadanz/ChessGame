3b9b0184a3f71b480ab40245c2ed777827c5d23d
Quinnvr <35711409+Quinnvr@users.noreply.github.com>
2018-07-21 17:38:31 -0600

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -9,7 +9,29 @@ public class Queen extends Piece{
 			this.name = "b" + name;
 	}
 	
+	Queen(int x, int y, boolean r)
+	{
+		super(x, y, "Queen", r);
+	}
+	
 	public String drawPiece() {
 		return name;
 	}
+	
+	public boolean isAvailable(int initX, int initY, int finX, int finY)
+	{
+		if (!super.isAvailable(int initX, int initY, int finX, int finY))
+		{
+			return false;
+		}
+
+		if ((finX == initX) || (finY == initY))
+		{
+			return true;
+		}
+		else if (Math.abs(initX - finX) == Math.abs(initY - finY))
+		{
+			return true;
+		}	
+
 }
