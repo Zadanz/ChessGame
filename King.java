9ad925fb60b1a265cdd51f14386ed1b60159bd53
Zadanz <41347579+Zadanz@users.noreply.github.com>
2018-07-21 20:58:18 -0600

    Update King.java

--- King.java
+++ King.java
@@ -12,14 +12,22 @@ public class King extends Piece{
 		return name;
 	}
 	
-	public boolean canMove(int x, int y){
-		if (board[x,y].name.CharAt(0) == this.name.CharAt(0))
+	public boolean inCheck(String[][] array, int x, int y) { //INCOMPLETE
+		return false;
+	}
+	
+	public boolean canMove(String[][] array, int x, int y){
+		if (array[x][y].charAt(0) == this.name.charAt(0))
 			return false;
 		
-		if ((x == this.posX+1 && y == this.posY)||(x == this.posX+1 && y == this.posY+1)||(x == this.posX && y == this.posY+1)||(x == this.posX-1 && y == this.posY)||(x == this.posX-1 && y == this.posY-1)||(x == this.posX && y == this.posY-1)||(x == this.posX+1 && y == this.posY-1)||(x == this.posX-1 && y == this.posY+1))
-			if (array[x,y].inCheck) //used to check whether the move will result in putting the king into check, thereby making it illegal. (This function doesn't exist yet.)
-				return false;
-			else
-				return true;
-	}	
+		else if ((x == this.posX+1 && y == this.posY)||(x == this.posX+1 && y == this.posY+1)||(x == this.posX && y == this.posY+1)||(x == this.posX-1 && y == this.posY)||(x == this.posX-1 && y == this.posY-1)||(x == this.posX && y == this.posY-1)||(x == this.posX+1 && y == this.posY-1)||(x == this.posX-1 && y == this.posY+1))
+				if (inCheck(array, x, y)) //used to check whether the move will result in putting the king into check, thereby making it illegal. (This function doesn't exist yet.)
+					return false;
+				else
+					return true;
+		
+		else
+			return false;
+		}
+	
 }
